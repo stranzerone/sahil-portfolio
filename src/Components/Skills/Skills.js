14039d7c3e49b3ff4e41dd72c4b138c0d18c456c
stranzerone <113999437+stranzerone@users.noreply.github.com>
2024-04-13 18:18:07 +0530

    bug2

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -9,36 +9,36 @@ function Skills() {
         
         <div className="flex flex-col items-center justify-center h-28 rounded-md bg-yellow-200 my-3 mx-1 flex-grow">
           <FaHtml5 className="text-2xl mb-1" style={{ color: "#091c49" }} />
-          <span className="font-bold text-md md:text-lg">HTML5</span>
+          <span className="font-bold text-sm md:text-lg">HTML5</span>
         </div>
 
         <div className="flex flex-col items-center justify-center h-28 rounded-md bg-yellow-200 my-3 mx-1 flex-grow">
           <FaCss3 className="text-2xl mb-1" style={{ color: "#4d1485" }} />
-          <span className="font-bold text-md md:text-lg">CSS3</span>
+          <span className="font-bold text-sm md:text-lg">CSS3</span>
         </div>
 
         <div className="flex flex-col items-center justify-center h-28 rounded-md bg-yellow-200 my-3 mx-1 flex-grow">
           <FaJs className="text-2xl mb-1" style={{ color: "#FFD43B" }} />
-          <span className="font-bold text-md md:text-lg">JavaScript</span>
+          <span className="font-bold text-sm md:text-lg">JavaScript</span>
         </div>
         <div className="flex flex-col items-center justify-center h-28 rounded-md bg-yellow-200 my-3 mx-1 flex-grow">
           <FaJs className="text-2xl mb-1" style={{ color: "#FFD43B" }} />
-          <span className="font-bold text-md md:text-lg">Bootstrap & Tailwind</span>
+          <span className="font-bold text-sm md:text-lg">Bootstrap & Tailwind</span>
         </div>
 
         <div className="flex flex-col items-center justify-center h-28 rounded-md bg-yellow-200 my-3 mx-1 flex-grow">
           <FaNode className="text-2xl mb-1" style={{ color: "#38e907" }} />
-          <span className="font-bold text-md md:text-lg">NodeJs</span>
+          <span className="font-bold text-sm md:text-lg">NodeJs</span>
         </div>
 
         <div className="flex flex-col items-center justify-center h-28 rounded-md bg-yellow-200 my-3 mx-1 flex-grow">
           <FaDatabase className="text-2xl mb-1" style={{ color: "#74C0FC" }} />
-          <span className="font-bold text-md md:text-lg">MongoDB & MySQL</span>
+          <span className="font-bold text-sm md:text-lg">MongoDB & MySQL</span>
         </div>
 
         <div className="flex flex-col items-center justify-center h-28 rounded-md bg-yellow-200 my-3 mx-1 flex-grow">
           <FaReact className="text-2xl mb-1" style={{ color: "#085ae7" }} />
-          <span className="font-bold text-md md:text-lg">ReactJs & React-native</span>
+          <span className="font-bold text-sm md:text-lg">ReactJs & React-native</span>
         </div>
 
       </div>
